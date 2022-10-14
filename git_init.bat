54ef6a87fd6c2f198de55dd897c851847e566f69
vengateshm <40466166+vengateshm@users.noreply.github.com>
2022-10-14 14:48:35 +0530

    initial commit

--- git_init.bat
+++ git_init.bat
@@ -2,5 +2,7 @@
 git init
 git add .
 git commit -m "initial commit"
-git branch
+git branch -M main
+git remote add origin https://github.com/vengateshm/AndroidPractice.git
+git push -u origin main
 pause
